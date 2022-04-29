3cfa4bc443bd173cf1aa834c1f623d3ab06ae77e
Paul-Nixon <nixonp4@gmail.com>
2022-04-28 19:03:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Profile from './Profile';
 
 
 export default function App() {
   return (
-    <div>
+    <div className='profiles-wrapper'>
 
     </div>
   )
--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
+import useToggle from './custom_hooks/useToggle';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Profile() {
+  const [isActive, toggleValue] = useToggle(false),
+  profileRef = useRef();
+
+  
   return (
-    <div>
-        
+    <div className='profile' ref={profileRef} onClick={() => {
+      if (!isActive)
+      {
+        toggleValue(!isActive);
+        profileRef.current.classList.add("active");
+      }
+    }}>
+        {isActive ? (
+          <>
+            <FontAwesomeIcon icon={faCircleUser} className="profile-img fa-fw" />
+            <span className='profile-name'>Andy Jones</span>
+          </>
+        ) : (
+          <div className='topbar'>
+            <div>
+              <FontAwesomeIcon icon={faCircleUser} className="profile-img fa-fw" />
+              <span className='profile-name'>Andy Jones</span>
+            </div>
+
+            <span>&times;</span>
+          </div>
+        )}
     </div>
   )
 }
